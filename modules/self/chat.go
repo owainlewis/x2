63aa44c201b03c661b9b4354ba613f9035f2fffc
Owain Lewis <owain@owainlewis.com>
2016-02-24 08:03:17 +0000

    use regex for more complex queries

--- modules/self/chat.go
+++ modules/self/chat.go
@@ -5,19 +5,37 @@ package self
 
 import (
 	"github.com/owainlewis/x2/agent"
+	"regexp"
 )
 
 type Self struct {
 }
 
+func greeting(input string) bool {
+	var expr = regexp.MustCompile(`hello|hi|hey`)
+	return expr.MatchString(input)
+}
+func feeling(input string) bool {
+	var expr = regexp.MustCompile(`how are you feeling`)
+	return expr.MatchString(input)
+}
+
 func (chat Self) Matches(input string) bool {
-	if input == "who are you" {
+	if greeting(input) {
+		return true
+	}
+	if feeling(input) {
 		return true
 	}
 	return false
 }
 
 func (chat Self) Perform(agent *agent.Agent) agent.AgentReply {
-	println(agent.LastQuery.Ask)
-	return agent.Reply("My name is " + agent.Name)
+	if greeting(agent.LastQuery.Ask) {
+		return agent.Reply("Hi. My name is " + agent.Name)
+	}
+	if feeling(agent.LastQuery.Ask) {
+		return agent.Reply("I'm great. Thanks for asking")
+	}
+	return agent.Reply("Pardon?")
 }
